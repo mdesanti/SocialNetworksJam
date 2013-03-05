98e3abe3c39f6624d6b0563061ec983c2e975db9
Matias De Santi <mdesanti90@gmail.com>
2013-03-05 17:35:12 -0300

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetworksJam::Application.config.secret_token = 'eabbffb8adc5d9279b729fbdb3132c0fd2c1bdf678673f6aa2aab89c1ffcb893551eca9c09b5e242538e0399a8a2f8172b73ceca79ae13610f513fc2d3f1941f'
